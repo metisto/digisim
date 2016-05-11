d7c1604c1da0ead9c8ef0ba97d851c55293c79e4
Olivier Albiez <olivier.albiez@poloper.org>
2016-05-11 14:51:05 +0200

    Make components public

--- src/main/java/metisto/digisim/Components.java
+++ src/main/java/metisto/digisim/Components.java
@@ -22,35 +22,35 @@ public final class Components {
         return wire;
     }
 
-    static BinaryGate and(final Connection a, final Connection b) {
+    public static BinaryGate and(final Connection a, final Connection b) {
         final BinaryGate and = new BinaryGate(BinaryGate.Operators.AND);
         and.a(a);
         and.b(b);
         return and;
     }
 
-    static BinaryGate or(final Connection a, final Connection b) {
+    public static BinaryGate or(final Connection a, final Connection b) {
         final BinaryGate or = new BinaryGate(BinaryGate.Operators.OR);
         or.a(a);
         or.b(b);
         return or;
     }
 
-    static BinaryGate xor(final Connection a, final Connection b) {
+    public static BinaryGate xor(final Connection a, final Connection b) {
         final BinaryGate xor = new BinaryGate(BinaryGate.Operators.XOR);
         xor.a(a);
         xor.b(b);
         return xor;
     }
 
-    static HalfAdder halfAdder(final Connection a, final Connection b) {
+    public static HalfAdder halfAdder(final Connection a, final Connection b) {
         final HalfAdder halfAdder = new HalfAdder();
         halfAdder.a(a);
         halfAdder.b(b);
         return halfAdder;
     }
 
-    static FullAdder fullAdder(final Connection a, final Connection b, final Connection r) {
+    public static FullAdder fullAdder(final Connection a, final Connection b, final Connection r) {
         final FullAdder fullAdder = new FullAdder();
         fullAdder.a(a);
         fullAdder.b(b);
